ec45e3536cc4a5ae3dfd7ca9ec283377304a3e8b
WarisDev <warissara13@gmail.com>
2020-03-12 11:52:04 +0700

    add pinno

---
+++ pinno.php
@@ -0,0 +1,69 @@
+<ion-view view-title="Soundboard">
+  <ion-content class="padding">
+		
+<div class="container">
+			<div class="row">
+
+				
+<div class="col-md-6 col-xs-6">
+					
+<audio preload="auto" id="sound1">
+						
+<source src="http://www.vibrationdata.com/Kawai_fourth_A_D.mp3" type="audio/mpeg">
+					
+</audio>
+					
+<button type="button" class="btn btn-lg btn-block" onclick="document.getElementById('sound1').play()">1</button>
+				</div>
+        
+        
+<div class="col-md-6 col-xs-6">
+					
+<audio preload="auto" id="sound1">
+						
+<source src="http://www.tremblingsandwarblings.com/wp-content/uploads/2017/05/piano-Cs4-long.mp3" type="audio/mpeg">
+					</audio>
+					<button type="button" class="btn btn-lg btn-block" onclick="document.getElementById('sound1').play()">2</button>
+				</div>
+        
+         <div class="col-md-6 col-xs-6">
+					<audio preload="auto" id="sound1">
+						<source src="https://freesound.org/data/previews/316/316907_5385832-lq.mp3" type="audio/mpeg">
+					</audio>
+					<button type="button" class="btn btn-lg btn-block" onclick="document.getElementById('sound1').play()">3</button>
+				</div>
+        
+        
+<div class="col-md-6 col-xs-6">
+					
+<audio preload="auto" id="sound1">
+						
+<source src="http://www.vibrationdata.com/piano_C_sharp.mp3" type="audio/mpeg">
+					</audio>
+					<button type="button" class="btn btn-lg btn-block" onclick="document.getElementById('sound1').play()">4</button>
+				</div>
+        
+         <div class="col-md-6 col-xs-6">
+					<audio preload="auto" id="sound1">
+						<source src="http://www.vibrationdata.com/Kawai_Major_Sixth_A_F_sharp.mp3" type="audio/mpeg">
+					</audio>
+					<button type="button" class="btn btn-lg btn-block" onclick="document.getElementById('sound1').play()">5</button>
+				</div>
+
+
+        
+        
+        
+        
+        
+			</div>
+
+		</div>
+		
+		<footer>
+			<div class="col-md-offset-9 col-xs-offset-9">
+		<!--	<p class="text">made with <a href="https://getbootstrap.com" target="_blank">bootstrap</a> and &#x2665;</p> -->
+			</div>
+		</footer>
+  </ion-content>
+</ion-view> 
